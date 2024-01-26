3aca78ae872ff20aad782f4beded341e56217d86
Lee <leesheppard2404@gmail.com>
2024-01-26 17:57:45 +0100

    feat: added page footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SocialIcons from './SocialIcons';
+
+const Footer = () => {
+  return (
+    <section className="bg-black object-bottom text-center">
+      <h4 className="pt-12 text-3xl tracking-widest">Lets Get Connected!</h4>
+      <SocialIcons />
+    </section>
+  );
+};
+
+export default Footer;
